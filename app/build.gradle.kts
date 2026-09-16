4375120fa5d4509e5a603c50db38b975d4efda9a
MixterVee <mixtervandal@gmail.com>
2026-09-16 19:52:43 -0400

    Bump Chatty Mic Test to v0.13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mixtervee.chattymicttest"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.12"
+        versionCode = 13
+        versionName = "0.13"
     }
 
     buildTypes {
